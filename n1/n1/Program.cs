e765b06e3359cd0b236e36690196bacbbd7d548f
ddw <2780624596@qq.com>
2021-07-08 10:16:26 +0800

    第一版

---
+++ n1/n1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace n1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+
+            Console.WriteLine("fack");
+        }
+    }
+}
